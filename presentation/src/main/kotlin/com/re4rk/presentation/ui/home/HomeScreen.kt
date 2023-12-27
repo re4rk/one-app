efce84f723ac1070569c2b678e21981e473625b8
re4rk <37167652+re4rk@users.noreply.github.com>
2023-12-27 10:10:16 +0900

    feat: 홈 화면 아이콘 그리드로 변경

--- presentation/src/main/kotlin/com/re4rk/presentation/ui/home/HomeScreen.kt
+++ presentation/src/main/kotlin/com/re4rk/presentation/ui/home/HomeScreen.kt
@@ -1,22 +1,28 @@
 package com.re4rk.presentation.ui.home
 
 import android.annotation.SuppressLint
+import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.absolutePadding
+import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
 import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Scaffold
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import com.re4rk.presentation.ui.chatRoomList.ChatRoomListActivity
@@ -36,33 +42,48 @@ private fun HomeScreen(contents: List<Content>) = OneAppTheme(
 ) {
     Scaffold(
         modifier = Modifier.fillMaxHeight(),
-        content = {
-            LazyColumn {
-                contents.forEach { content ->
-                    item {
-                        CategoryItem(content)
-                    }
-                }
-            }
-        },
+        content = { HomeScreenContent(contents) },
     )
 }
 
 @Composable
+private fun HomeScreenContent(contents: List<Content>) {
+    LazyVerticalGrid(columns = GridCells.Adaptive(minSize = 128.dp)) {
+        contents.forEach { content ->
+            item {
+                CategoryItem(content = content)
+            }
+        }
+    }
+}
+
+@Composable
 private fun CategoryItem(
+    modifier: Modifier = Modifier,
     content: Content,
 ) {
     val context = LocalContext.current
 
     HomeCard(
         onClick = { context.startActivity(content.intent) },
-        modifier = Modifier.padding(8.dp),
+        modifier = modifier.padding(8.dp),
     ) {
         Column(
-            modifier = Modifier.absolutePadding(left = 16.dp),
+            modifier = modifier.padding(12.dp),
             verticalArrangement = Arrangement.Center,
+            horizontalAlignment = Alignment.CenterHorizontally,
         ) {
-            Text(text = content.description)
+            Image(
+                imageVector = Icons.Default.Close,
+                contentDescription = null,
+            )
+
+            Spacer(modifier = Modifier.height(8.dp))
+
+            Text(
+                text = content.description,
+                overflow = TextOverflow.Ellipsis,
+            )
         }
     }
 }
@@ -70,13 +91,15 @@ private fun CategoryItem(
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 private fun HomeCard(
-    onClick: () -> Unit = { },
     modifier: Modifier = Modifier,
+    onClick: () -> Unit = { },
     content: @Composable () -> Unit,
 ) {
     Surface(
         onClick = onClick,
-        modifier = modifier.fillMaxWidth().height(64.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .height(96.dp),
         shape = RoundedCornerShape(12.dp),
         tonalElevation = 4.dp,
         content = content,
@@ -85,8 +108,8 @@ private fun HomeCard(
 
 @Preview
 @Composable
-fun previewHomeSuccess() {
-    HomeRoute(
+fun PreviewHomeSuccess() {
+    HomeScreen(
         contents = listOf(
             Content(
                 intent = ChatRoomListActivity.getIntent(LocalContext.current),
@@ -96,6 +119,26 @@ fun previewHomeSuccess() {
                 intent = LifecycleTackerActivity.getIntent(LocalContext.current),
                 description = "Lifecycle Tracker",
             ),
+            Content(
+                intent = LifecycleTackerActivity.getIntent(LocalContext.current),
+                description = "Lifecycle Tracker",
+            ),
+            Content(
+                intent = LifecycleTackerActivity.getIntent(LocalContext.current),
+                description = "Lifecycle Tracker",
+            ),
+        ),
+    )
+}
+
+@Preview
+@Composable
+fun PreviewCategoryItem() {
+    CategoryItem(
+        modifier = Modifier.padding(8.dp),
+        content = Content(
+            intent = ChatRoomListActivity.getIntent(LocalContext.current),
+            description = "Chat Room List",
         ),
     )
 }
