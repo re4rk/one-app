2c7e976809091e48c65a228758869e02ee3b96d1
re4rk <37167652+re4rk@users.noreply.github.com>
2024-01-01 03:20:02 +0900

    feat(app) : 화면 네비게이션에 목적지 enum 클래스 추가

--- app/src/main/java/com/re4rk/app/MainActivity.kt
+++ app/src/main/java/com/re4rk/app/MainActivity.kt
@@ -1,19 +1,35 @@
 package com.re4rk.app
 
+import android.annotation.SuppressLint
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Close
+import androidx.compose.material3.Scaffold
+import androidx.compose.material3.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.navigation.NavController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.rememberNavController
 import com.re4rk.app.ui.theme.OneAppTheme
+import com.re4rk.navigation.TopLevelDestination
+import com.re4rk.presentation.designSystem.component.ArkNavigationBar
+import com.re4rk.presentation.designSystem.component.ArkNavigationBarItem
 import com.re4rk.presentation.ui.chatRoomList.ChatRoomListActivity
 import com.re4rk.presentation.ui.diPractice.DiPracticeActivity
 import com.re4rk.presentation.ui.home.Content
 import com.re4rk.presentation.ui.home.navigation.homeNavigationRoute
 import com.re4rk.presentation.ui.home.navigation.homeScreen
+import com.re4rk.presentation.ui.home.navigation.navigateToHome
 import com.re4rk.presentation.ui.lifecycleTracker.LifecycleTackerActivity
 import com.re4rk.presentation.ui.memo.MemoActivity
 
+@SuppressLint("UnusedMaterial3ScaffoldPaddingParameter")
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
@@ -22,13 +38,48 @@ class MainActivity : ComponentActivity() {
             val navController = rememberNavController()
 
             OneAppTheme {
-                NavHost(
-                    navController = navController,
-                    startDestination = homeNavigationRoute,
+                Scaffold(
+                    bottomBar = { MainScreenBottomBar(navController) },
                 ) {
-                    homeScreen(contents = getFakeHomeScreenContents(this@MainActivity))
+                    NavHost(
+                        navController = navController,
+                        startDestination = homeNavigationRoute,
+                    ) {
+                        homeScreen(contents = getFakeHomeScreenContents(this@MainActivity))
+                    }
                 }
             }
+        }
+    }
+
+    @Composable
+    private fun MainScreenBottomBar(
+        navController: NavController,
+    ) = ArkNavigationBar(
+        modifier = Modifier
+            .fillMaxWidth()
+            .wrapContentHeight(),
+    ) {
+        TopLevelDestination.values().forEach { destination ->
+            ArkNavigationBarItem(
+                selected = true,
+                onClick = {
+                    when (destination) {
+                        TopLevelDestination.HOME -> {
+                            navController.navigateToHome()
+                        }
+                    }
+                },
+                icon = {
+                    Image(
+                        imageVector = Icons.Default.Close,
+                        contentDescription = null,
+                    )
+                },
+                label = {
+                    Text(text = destination.name)
+                },
+            )
         }
     }
 
