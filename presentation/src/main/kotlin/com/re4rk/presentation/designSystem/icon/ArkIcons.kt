326b52db844a7d6c6969ce4d1d39965e3a8550d4
re4rk <37167652+re4rk@users.noreply.github.com>
2024-01-01 01:34:01 +0900

    feat(designSystem) : 앱에서 사용할 아이콘 추가

---
+++ presentation/src/main/kotlin/com/re4rk/presentation/designSystem/icon/ArkIcons.kt
@@ -0,0 +1,26 @@
+package com.re4rk.presentation.designSystem.icon
+
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Home
+import androidx.compose.material.icons.filled.MoreVert
+import androidx.compose.material.icons.rounded.Add
+import androidx.compose.material.icons.rounded.ArrowBack
+import androidx.compose.material.icons.rounded.Check
+import androidx.compose.material.icons.rounded.Close
+import androidx.compose.material.icons.rounded.List
+import androidx.compose.material.icons.rounded.Person
+import androidx.compose.material.icons.rounded.Search
+import androidx.compose.material.icons.rounded.Settings
+
+object ArkIcons {
+    val Add = Icons.Rounded.Add
+    val ArrowBack = Icons.Rounded.ArrowBack
+    val Check = Icons.Rounded.Check
+    val Close = Icons.Rounded.Close
+    val Home = Icons.Default.Home
+    val List = Icons.Rounded.List
+    val MoreVert = Icons.Default.MoreVert
+    val Person = Icons.Rounded.Person
+    val Search = Icons.Rounded.Search
+    val Settings = Icons.Rounded.Settings
+}
