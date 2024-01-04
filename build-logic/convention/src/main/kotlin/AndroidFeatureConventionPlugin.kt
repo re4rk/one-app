593e599774abb7cd64f4bf71e9646d2cdb69065f
re4rk <37167652+re4rk@users.noreply.github.com>
2024-01-05 04:05:13 +0900

    chore(presentation): 빌드 로직을 활용하여 빌드하도록 변경

---
+++ build-logic/convention/src/main/kotlin/AndroidFeatureConventionPlugin.kt
@@ -0,0 +1,29 @@
+import com.re4rk.oneApp.libs
+import org.gradle.api.Plugin
+import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
+import org.gradle.kotlin.dsl.kotlin
+
+class AndroidFeatureConventionPlugin : Plugin<Project> {
+    override fun apply(target: Project) {
+        with(target) {
+            pluginManager.apply {
+                apply("oneApp.android.library")
+                apply("oneApp.android.hilt")
+            }
+
+            dependencies {
+                add("implementation", project(":domain"))
+
+                add("testImplementation", kotlin("test"))
+                add("androidTestImplementation", kotlin("test"))
+
+                add("implementation", libs.findLibrary("androidx.hilt.navigation.compose").get())
+                add("implementation", libs.findLibrary("androidx.lifecycle.runtimeCompose").get())
+                add("implementation", libs.findLibrary("androidx.lifecycle.viewModelCompose").get())
+
+                add("implementation", libs.findLibrary("kotlinx.coroutines.android").get())
+            }
+        }
+    }
+}
--- presentation/build.gradle.kts
+++ presentation/build.gradle.kts
@@ -1,145 +1,31 @@
 plugins {
-    id("com.android.library")
-    id("org.jetbrains.kotlin.android")
-    id("kotlin-parcelize")
-    id("kotlin-kapt")
-    id("com.google.dagger.hilt.android")
-    kotlin("plugin.serialization") version "1.8.22"
+    id("oneApp.android.feature")
+    id("oneApp.android.library.compose")
 }
 
 android {
     namespace = "com.re4rk.presentation"
-    compileSdk = 34
-
-    defaultConfig {
-        minSdk = 28
-        targetSdk = 34
-
-        testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
-        vectorDrawables {
-            useSupportLibrary = true
-        }
-    }
-
-    buildTypes {
-        release {
-            isMinifyEnabled = false
-            proguardFiles(
-                getDefaultProguardFile("proguard-android-optimize.txt"),
-                "proguard-rules.pro",
-            )
-        }
-    }
-    compileOptions {
-        sourceCompatibility = JavaVersion.VERSION_17
-        targetCompatibility = JavaVersion.VERSION_17
-    }
-    kotlinOptions {
-        jvmTarget = "17"
-    }
-
-    dataBinding {
-        enable = true
-    }
 
     android.sourceSets.all {
         kotlin.srcDir("src/$name/kotlin")
-    }
-
-    testOptions {
-        unitTests.isReturnDefaultValues = true
-    }
-    buildFeatures {
-        compose = true
-    }
-    composeOptions {
-        kotlinCompilerExtensionVersion = "1.4.6"
-    }
-    packaging {
-        resources {
-            excludes += "/META-INF/{AL2.0,LGPL2.1}"
-        }
     }
 }
 
 dependencies {
     // android
-    implementation("androidx.core:core-ktx:1.10.1")
-    implementation("androidx.appcompat:appcompat:1.6.1")
-    implementation("com.google.android.material:material:1.9.0")
-    implementation("androidx.constraintlayout:constraintlayout:2.1.4")
-
-    // recyclerview
-    implementation("androidx.recyclerview:recyclerview:1.3.1")
-
-    // lifecycle
-    implementation("androidx.lifecycle:lifecycle-extensions:2.2.0")
-    implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0-alpha01")
-    implementation("androidx.lifecycle:lifecycle-runtime-compose:2.7.0-alpha01")
+    implementation(libs.androidx.core.ktx)
+    implementation(libs.android.material)
 
     // glide
-    implementation("com.github.bumptech.glide:glide:4.15.1")
-
-    // retrofit
-    implementation("com.squareup.retrofit2:retrofit:2.9.0")
-
-    // kotlin-serialization
-    implementation("com.jakewharton.retrofit:retrofit2-kotlinx-serialization-converter:1.0.0")
-    implementation("org.jetbrains.kotlinx:kotlinx-serialization-json:1.5.1")
+    implementation(libs.glide)
+    implementation(libs.glide.landscapist)
 
     // compose
-    implementation("androidx.lifecycle:lifecycle-runtime-ktx:2.6.1")
-    implementation("androidx.activity:activity-compose:1.7.2")
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.compose.ui:ui")
-    implementation("androidx.compose.ui:ui-graphics")
-    implementation("androidx.compose.ui:ui-tooling-preview")
-    implementation("androidx.compose.material3:material3")
-    implementation(project(mapOf("path" to ":domain")))
-    implementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    implementation("androidx.navigation:navigation-runtime-ktx:2.7.6")
-    implementation("androidx.navigation:navigation-compose:2.7.6")
-
-    // hiltViewModel compose
-    implementation("androidx.hilt:hilt-navigation-compose:1.1.0")
-
-    // junit4
-    testImplementation("junit:junit:4.13.2")
-    testImplementation("androidx.test.ext:junit:1.1.5")
-    testImplementation("androidx.test:runner:1.5.2")
-
-    // android-test
-    testImplementation("androidx.arch.core:core-testing:2.2.0")
-
-    // mock
-    testImplementation("io.mockk:mockk-android:1.13.5")
-
-    // espresso
-    androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
+    implementation(libs.androidx.compose.ui.graphic)
+    implementation(libs.androidx.compose.ui.tooling.preview)
+    implementation(libs.androidx.compose.material3)
+    implementation(libs.androidx.compose.material.iconsExtended)
 
     // coroutine
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.7.2")
-    implementation("org.jetbrains.kotlinx:kotlinx-coroutines-jdk8:1.7.2")
-    testImplementation("org.jetbrains.kotlinx:kotlinx-coroutines-test:1.7.2")
-
-    // viewModel
-    implementation("androidx.lifecycle:lifecycle-viewmodel-ktx:2.6.1")
-    implementation("androidx.activity:activity-ktx:1.7.2")
-    implementation("androidx.fragment:fragment-ktx:1.6.1")
-
-    // zxing
-    implementation("com.journeyapps:zxing-android-embedded:4.3.0")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    androidTestImplementation("androidx.compose.ui:ui-test-junit4")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
-    debugImplementation("androidx.compose.ui:ui-tooling")
-    debugImplementation("androidx.compose.ui:ui-test-manifest")
-
-    // glide
-    implementation("com.github.skydoves:landscapist-glide:1.4.7")
-
-    // Hilt
-    implementation("com.google.dagger:hilt-android:2.47")
-    kapt("com.google.dagger:hilt-android-compiler:2.47")
-    kapt("androidx.hilt:hilt-compiler:1.0.0")
+    implementation(libs.kotlinx.coroutines.android)
 }
