3e5402b5684f6bd9a8ae9a233c15e48f3d87c54e
re4rk <37167652+re4rk@users.noreply.github.com>
2024-01-06 00:34:14 +0900

    feature(shopping): 쇼핑 화면 추가

---
+++ feature/shopping/build.gradle.kts
@@ -0,0 +1,24 @@
+
+plugins {
+    id("oneApp.android.feature")
+    id("oneApp.android.library.compose")
+}
+
+android {
+    namespace = "com.re4rk.oneapp.feature.shopping"
+
+    android.sourceSets.all {
+        kotlin.srcDir("src/$name/kotlin")
+    }
+}
+
+dependencies {
+    implementation(project(":core:designsystem"))
+
+    // glide
+    implementation(libs.glide)
+    implementation(libs.glide.landscapist)
+
+    // coroutine
+    implementation(libs.kotlinx.coroutines.android)
+}
