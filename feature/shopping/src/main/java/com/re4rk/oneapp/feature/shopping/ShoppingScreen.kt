4e171dfce9a6918a4fb16e81224875b03833d56f
re4rk <37167652+re4rk@users.noreply.github.com>
2024-01-13 10:57:15 +0900

    feat(shopping): 장바구니 컴포저블 아이템을 활용하는 스크린 추가

---
+++ feature/shopping/src/main/java/com/re4rk/oneapp/feature/shopping/ShoppingScreen.kt
@@ -0,0 +1,61 @@
+package com.re4rk.oneapp.feature.shopping
+
+import androidx.compose.foundation.background
+import androidx.compose.foundation.clickable
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.lazy.grid.GridCells
+import androidx.compose.foundation.lazy.grid.LazyVerticalGrid
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ShoppingRoot() {
+    ShoppingScreen()
+}
+
+@Composable
+fun ShoppingScreen() {
+    LazyVerticalGrid(
+        modifier =
+        Modifier
+            .background(
+                MaterialTheme.colorScheme.primary,
+            )
+            .padding(8.dp)
+            .fillMaxHeight(),
+        columns = GridCells.Adaptive(minSize = 128.dp),
+    ) {
+        repeat(10) {
+            item {
+                val count = remember { mutableStateOf(2) }
+                ShoppingItem(
+                    modifier = Modifier
+                        .fillMaxWidth()
+                        .height(250.dp)
+                        .padding(8.dp)
+                        .clickable { },
+                    imageUrl = "https://picsum.photos/600/600",
+                    title = "Title",
+                    price = "Price",
+                    description = "Description",
+                    count = count.value,
+                    countChangeListener = { count.value = it },
+                )
+            }
+        }
+    }
+}
+
+@Composable
+@Preview
+fun ShoppingRootPreview() {
+    ShoppingRoot()
+}
