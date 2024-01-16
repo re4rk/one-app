03d944456209db2a5520acedb5d08a4b8d921427
re4rk <37167652+re4rk@users.noreply.github.com>
2024-01-16 14:34:12 +0900

    chore(shopping-detail): 쇼핑 디테일 모듈 추가

--- feature/shopping-detail/src/main/java/com/re4rk/oneapp/feature/shoppingdetail/ShoppingDetailScreen.kt
+++ feature/shopping-detail/src/main/java/com/re4rk/oneapp/feature/shoppingdetail/ShoppingDetailScreen.kt
@@ -1,16 +1,69 @@
 package com.re4rk.oneapp.feature.shoppingdetail
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.padding
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.re4rk.oneapp.core.designsystem.component.DynamicAsyncImage
+import com.re4rk.oneapp.domain.model.Product
 
 @Composable
-fun ShoppingDetailScreen() {
-    Text(text = "ShoppingDetailScreen")
+fun ShoppingDetailScreen(
+    product: Product,
+) {
+    Column(
+        modifier = Modifier
+            .fillMaxWidth()
+            .fillMaxHeight(),
+    ) {
+        DynamicAsyncImage(
+            imageUrl = product.imageUrl,
+            modifier = Modifier
+                .fillMaxWidth()
+                .height(250.dp),
+            contentDescription = null,
+        )
+
+        Column(
+            modifier = Modifier
+                .padding(horizontal = 8.dp),
+        ) {
+            Text(
+                text = product.title,
+                style = MaterialTheme.typography.headlineLarge,
+            )
+            Text(
+                text = product.description,
+                style = MaterialTheme.typography.titleLarge,
+            )
+            Text(
+                text = product.price,
+                style = MaterialTheme.typography.bodyLarge,
+            )
+        }
+    }
 }
 
 @Composable
 @Preview
 fun ShoppingDetailScreenPreview() {
-    ShoppingDetailScreen()
+    Surface {
+        ShoppingDetailScreen(
+            product = Product(
+                id = 1,
+                title = "name",
+                description = "description",
+                price = "100",
+                imageUrl = "https://picsum.photos/600/600",
+            ),
+        )
+    }
 }
