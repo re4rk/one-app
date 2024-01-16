0446113073f5c5d73c98aaeb5eae5d02971bd62c
re4rk <37167652+re4rk@users.noreply.github.com>
2024-01-16 16:44:47 +0900

    chore(project): kotlin 라이브러리 hilt core 의존성 추가

--- build-logic/convention/src/main/kotlin/KotlinLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/KotlinLibraryConventionPlugin.kt
@@ -1,7 +1,8 @@
-
 import com.re4rk.oneApp.configureKotlinJvm
+import com.re4rk.oneApp.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.kotlin.dsl.dependencies
 
 class KotlinLibraryConventionPlugin : Plugin<Project> {
     override fun apply(target: Project) {
@@ -9,6 +10,10 @@ class KotlinLibraryConventionPlugin : Plugin<Project> {
             with(pluginManager) {
                 apply("java-library")
                 configureKotlinJvm()
+            }
+
+            dependencies {
+                add("implementation", libs.findLibrary("hilt.core").get())
             }
         }
     }
