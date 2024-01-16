85098e3954bb3572e631aad81fe013e6a556bd98
re4rk <37167652+re4rk@users.noreply.github.com>
2024-01-16 13:02:25 +0900

    refactor(data): 내부 변수 노출

--- domain/src/main/java/com/re4rk/oneapp/domain/model/CartProduct.kt
+++ domain/src/main/java/com/re4rk/oneapp/domain/model/CartProduct.kt
@@ -1,17 +1,6 @@
 package com.re4rk.oneapp.domain.model
 
 data class CartProduct(
-    private val product: Product,
+    val product: Product,
     val count: Int,
-) {
-    val title: String
-        get() = product.title
-    val description: String
-        get() = product.description
-    val price: String
-        get() = product.price
-    val imageUrl: String
-        get() = product.imageUrl
-    val productId: Long
-        get() = product.id
-}
+)
