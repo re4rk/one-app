263994099a6226760ad70a7ec2b6169182e49e64
re4rk <37167652+re4rk@users.noreply.github.com>
2024-01-16 12:36:18 +0900

    feat(data): ShoppingRepository 인터페이스 추가

---
+++ domain/src/main/java/com/re4rk/oneapp/domain/repository/ShoppingRepository.kt
@@ -0,0 +1,12 @@
+package com.re4rk.oneapp.domain.repository
+
+import com.re4rk.oneapp.domain.model.CartProduct
+import com.re4rk.oneapp.domain.model.Product
+import kotlinx.coroutines.flow.Flow
+
+interface ShoppingRepository {
+    val products: Flow<List<Product>>
+    val cartProducts: Flow<List<CartProduct>>
+
+    fun changeCartProductCount(productId: Long, count: Int)
+}
