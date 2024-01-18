ff65ac735f291a26293cb06f8f3aca3c0751db68
re4rk <37167652+re4rk@users.noreply.github.com>
2024-01-18 19:41:02 +0900

    chore(network-coinone) : 의존성 추가

--- core/network-coinone/build.gradle.kts
+++ core/network-coinone/build.gradle.kts
@@ -1,6 +1,12 @@
 plugins {
     id("oneApp.kotlin.library")
+    kotlin("plugin.serialization") version "1.8.22"
 }
 dependencies {
+    implementation(libs.kotlinx.coroutines.core)
+    implementation(libs.okhttp)
+    implementation(libs.okhttp.logging)
     implementation(libs.retrofit.core)
+    implementation(libs.kotlinx.serialization.json)
+    implementation(libs.retrofit.kotlin.serialization)
 }
