db05e3db71f0f7c1e8a425197b5c4e5c6195f403
re4rk <37167652+re4rk@users.noreply.github.com>
2024-01-18 18:20:37 +0900

    chore(build-logic): junit5, assertj 의존성 제공

--- build-logic/convention/src/main/kotlin/KotlinLibraryConventionPlugin.kt
+++ build-logic/convention/src/main/kotlin/KotlinLibraryConventionPlugin.kt
@@ -2,6 +2,7 @@ import com.re4rk.oneApp.configureKotlinJvm
 import com.re4rk.oneApp.libs
 import org.gradle.api.Plugin
 import org.gradle.api.Project
+import org.gradle.api.tasks.testing.Test
 import org.gradle.kotlin.dsl.dependencies
 
 class KotlinLibraryConventionPlugin : Plugin<Project> {
@@ -9,11 +10,21 @@ class KotlinLibraryConventionPlugin : Plugin<Project> {
         with(target) {
             with(pluginManager) {
                 apply("java-library")
+                apply("org.jetbrains.kotlin.jvm")
                 configureKotlinJvm()
+            }
+
+            project.tasks.withType(Test::class.java) {
+                useJUnitPlatform()
             }
 
             dependencies {
                 add("implementation", libs.findLibrary("hilt.core").get())
+
+                add("testImplementation", libs.findLibrary("assertj.core").get())
+
+                add("testImplementation", libs.findLibrary("junit5.jupiter.api").get())
+                add("testRuntimeOnly", libs.findLibrary("junit5.jupiter.engine").get())
             }
         }
     }
