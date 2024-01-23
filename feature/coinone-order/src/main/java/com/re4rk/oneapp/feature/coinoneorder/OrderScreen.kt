04f2e11366ad868e338517b72198171153e8dce9
re4rk <37167652+re4rk@users.noreply.github.com>
2024-01-24 00:59:13 +0900

    feat(feature-coinoneorder): 주문 페이지 탭바 추가

--- feature/coinone-order/src/main/java/com/re4rk/oneapp/feature/coinoneorder/OrderScreen.kt
+++ feature/coinone-order/src/main/java/com/re4rk/oneapp/feature/coinoneorder/OrderScreen.kt
@@ -9,14 +9,20 @@ import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.size
-import androidx.compose.foundation.layout.width
 import androidx.compose.foundation.layout.wrapContentHeight
+import androidx.compose.foundation.shape.RoundedCornerShape
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
+import androidx.compose.material3.Tab
+import androidx.compose.material3.TabRow
+import androidx.compose.material3.TabRowDefaults.tabIndicatorOffset
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.clip
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
@@ -140,6 +146,37 @@ private fun OrderScreenTop(
                 .background(Color(0xFF1763B6))
                 .size(width = 114.dp, height = 64.dp),
         ) {
+        }
+    }
+    val selected = remember { mutableStateOf(0) }
+    val tabs = listOf("주문", "호가", "차트", "시세", "정보")
+
+    TabRow(
+        selectedTabIndex = selected.value,
+        indicator = { tabPositions ->
+            Box(
+                modifier = Modifier
+                    .tabIndicatorOffset(tabPositions[selected.value])
+                    .height(4.dp)
+                    .clip(RoundedCornerShape(8.dp)) // clip modifier not working
+                    .padding(horizontal = 28.dp)
+                    .background(color = Color.Black),
+            )
+        },
+    ) {
+        tabs.forEachIndexed { index, tab ->
+            Tab(
+                text = {
+                    Text(
+                        text = tab,
+                        style = MaterialTheme.typography.labelLarge,
+                        fontSize = 14.sp,
+                        color = if (index == selected.value) Color(0xff000000) else Color(0xFFAEB3BB),
+                    )
+                },
+                selected = index == selected.value,
+                onClick = { selected.value = index },
+            )
         }
     }
 }
